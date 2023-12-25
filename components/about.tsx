f10b792c99fa7823cd0d3bfec913bfa8e47b717c
Arbiona <arbiona.shahu@gmail.com>
2023-12-25 08:05:48 +0100

    Add `About me` section

---
+++ components/about.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
+
+export default function About() {
+	return (
+		<motion.section
+			className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40"
+			initial={{ opacity: 0, y: 100 }}
+			animate={{ opacity: 1, y: 0 }}
+			transition={{ delay: 0.175 }}
+		>
+			<SectionHeading>About me</SectionHeading>
+			<p className="mb-3">
+				After graduating with a degree in{" "}
+				<span className="font-medium">Architecture</span>, I decided to pursue
+				my passion for programming. I enrolled in a coding bootcamp and learned{" "}
+				<span className="font-medium">full-stack web development</span>.{" "}
+				<span className="italic">My favorite part of programming</span> is the
+				problem-solving aspect. I <span className="underline">love</span> the
+				feeling of finally figuring out a solution to a problem. My core stack
+				is{" "}
+				<span className="font-medium">
+					React, Next.js, Node.js, and MongoDB
+				</span>
+				. I am also familiar with TypeScript and GraphQL. I am always looking to
+				learn new technologies.
+			</p>
+			<p>
+				<span className="italic">When I'm not coding</span>, I enjoy cooking,
+				painting, and doing crafts. Anything that involves building with hands.
+				I also enjoy <span className="font-medium">learning new AI tools</span>.
+				I am currently learning about{" "}
+				<span className="font-medium">Machine Learning</span> from
+				<span className="font-medium"> DeepLearning.ai</span>. I'm quite of
+				human centered person, and I constanly meditate and explore my inner
+				world. I can be quite nerdy about neuroscience.
+			</p>
+		</motion.section>
+	);
+}
--- components/intro.tsx
+++ components/intro.tsx
@@ -49,7 +49,7 @@ export default function Intro() {
 				</div>
 			</div>
 
-			<motion.p
+			<motion.h1
 				className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
 				initial={{ opacity: 0, y: 100 }}
 				animate={{ opacity: 1, y: 0 }}
@@ -59,7 +59,7 @@ export default function Intro() {
 				<span className="font-bold">+3 years</span> of experience. I enjoy
 				building <span className="italic">SAAS</span>. My focus is{" "}
 				<span className="underline">React (Next.js)</span>.
-			</motion.p>
+			</motion.h1>
 
 			<motion.div
 				className="flex flex-col sm:flex-row  items-center justify-center gap-2 px-4 text-lg font-medium"
