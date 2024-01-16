cf04667ae6a1ac9380b6b2c817577ba949c8cdcb
Arbiona <arbiona.shahu@gmail.com>
2024-01-16 20:50:14 +0100

    Add `Skills` section

---
+++ components/skills.tsx
@@ -0,0 +1,49 @@
+"use client";
+import React from "react";
+import SectionHeading from "./section-heading";
+import { skillsData } from "@/lib/data";
+import { motion } from "framer-motion";
+
+const fadeInAnimationVariants = {
+	initial: {
+		opacity: 0,
+		y: 100,
+	},
+	animate: (index: number) => ({
+		opacity: 1,
+		y: 0,
+		transition: {
+			delay: 0.07 * index, // delay based on items
+		},
+	}),
+};
+
+export default function Skills() {
+	return (
+		<section
+			id="skills"
+			className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40"
+		>
+			<SectionHeading>My skills</SectionHeading>
+			<ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-700">
+				{skillsData.map((skill, index) => (
+					<motion.li
+						className="bg-white border border-black/[0.1] rounded-xl px-5 py-3"
+						key={index}
+						variants={fadeInAnimationVariants}
+						initial="initial"
+						whileInView="animate"
+						viewport={{
+							once: true,
+						}}
+						custom={index}
+					>
+						{skill}
+					</motion.li>
+				))}
+			</ul>
+		</section>
+	);
+}
+
+// animate="animate" if we do it like this, it will animate as soon as we load the page
