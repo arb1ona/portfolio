e3b58d52fae96eb22a55f7bf2313511353fd7145
Arbiona <arbiona.shahu@gmail.com>
2024-01-16 20:50:40 +0100

    Add padding, comments for future ref

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,11 +1,15 @@
+// "use client"
 import React from "react";
 import SectionHeading from "./section-heading";
 import { projects } from "@/lib/data";
 import Project from "./project";
 
 export default function Projects() {
+	// const {ref} = useSectionInView("Skills")
+	// attach ref={ref} into section
+
 	return (
-		<section id="projects" className="scroll-mt-28">
+		<section id="projects" className="scroll-mt-28 mb-28">
 			<SectionHeading>My projects</SectionHeading>
 			<div>
 				{projects.map((project, index) => (
