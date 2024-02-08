daa47554087c318d945581ccc439e16086f8c797
Arbiona <arbiona.shahu@gmail.com>
2024-02-08 11:19:04 +0100

    Set up `Contact me` section

---
+++ components/contact.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import { FaPaperPlane } from "react-icons/fa";
+import { motion } from "framer-motion";
+
+function Contact() {
+	// const {ref} = useSectionInView("Contact")
+	// attach ref={ref} into section
+	return (
+		// pick the smallest
+		<motion.section
+			id="contact"
+			className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center"
+			initial={{
+				opacity: 0,
+			}}
+			whileInView={{ opacity: 1 }}
+			transition={{ duration: 1 }}
+			viewport={{ once: true }}
+		>
+			<SectionHeading>Contact me</SectionHeading>
+			<p className="text-gray-700 -mt-6">
+				Please contact me directly at{" "}
+				<a className="underline" href="mailto:arbiona.shahu@gmail.com">
+					arbiona.shahu@gmail.com
+				</a>{" "}
+				or through thus form.
+			</p>
+			<form className="mt-10 flex flex-col">
+				<input
+					className="h-14 rounded-lg borderBlack"
+					type="email"
+					placeholder="Your email"
+				/>
+				<textarea
+					className="h-52 my-3 rounded-lg borderBlack p-4"
+					placeholder="Your message"
+				/>
+				<button
+					className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active-scale-105"
+					type="submit"
+				>
+					Submit{" "}
+					<FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />
+				</button>
+			</form>
+		</motion.section>
+	);
+}
+
+export default Contact;
