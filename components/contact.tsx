e7e02207a4199d203c174a65a45e80fbac0489d5
Arbiona <arbiona.shahu@gmail.com>
2024-02-10 09:40:41 +0100

    Shows `loading` on send & `disables` button

--- components/contact.tsx
+++ components/contact.tsx
@@ -2,13 +2,14 @@
 
 import React from "react";
 import SectionHeading from "./section-heading";
-import { FaPaperPlane } from "react-icons/fa";
 import { motion } from "framer-motion";
 import { sendEmail } from "@/actions/sendEmail";
+import SubmitBtn from "./submit-btn";
 
 function Contact() {
 	// const {ref} = useSectionInView("Contact")
 	// attach ref={ref} into section
+
 	return (
 		// pick the smallest
 		<motion.section
@@ -50,13 +51,7 @@ function Contact() {
 					maxLength={5000}
 					placeholder="Your message"
 				/>
-				<button
-					className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active-scale-105"
-					type="submit"
-				>
-					Submit{" "}
-					<FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />
-				</button>
+				<SubmitBtn />
 			</form>
 		</motion.section>
 	);
