bf25d507cce4e148c80ab413f7c454bf73f79ede
Arbiona <arbiona.shahu@gmail.com>
2024-02-10 20:43:50 +0100

    Responsivity fixes

--- components/project.tsx
+++ components/project.tsx
@@ -31,11 +31,11 @@ export default function Project({
 		>
 			<section
 				className=" relative bg-gray-100 max-w-[42rem] borderBlack/5 rounded-lg overflow-hidden
-    sm:pr-8 sm:h-[20rem] hover:bg-gray-200 transition group-even:pl-8"
+    sm:pr-8 sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8"
 			>
 				<div
 					className="pt-4 pb-7 py-4 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full
-      group-even:ml-[18rem]"
+      sm:group-even:ml-[18rem]"
 				>
 					<h3 className="text-2xl font-semibold">{title}</h3>
 					<p className="mt-2 leading-relaxed text-gray-700">{description}</p>
@@ -55,7 +55,7 @@ export default function Project({
 					src={imageUrl}
 					alt="Project I worked on"
 					quality={95}
-					className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl 
+					className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl 
         group-hover:-translate-x-3
         group-hover:translate-y-3
         group-hover:-rotate-2
